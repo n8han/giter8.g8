24d8a694408e091a067ea19d0a214e8c53df6116
Nathan Hamblen <nathan@technically.us>
2010-08-16 00:21:58 -0400

    template template template ...

---
+++ project/build/project.scala
@@ -0,0 +1,3 @@
+import sbt._
+
+class Giter8Template(info: ProjectInfo) extends DefaultProject(info) with giter8.Template
---
+++ src/main/g8/project/build/project.scala
@@ -0,0 +1,3 @@
+import sbt._
+
+class TemplateProject(info: ProjectInfo) extends DefaultProject(info) with giter8.Template
