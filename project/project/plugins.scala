331559e3594e7e7b622765d5886730b0b9175fc2
Nathan Hamblen <nathan@technically.us>
2012-03-04 11:49:13 -0500

    depend on a version of the plugin that will resolve (but still won't work because it doesn't support ls(...))

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 object PluginDef extends Build {
   lazy val root = Project("plugins", file(".")) dependsOn( g8plugin )
   lazy val g8plugin =
-    ProjectRef(uri("git://github.com/n8han/giter8#0.4.0"), "giter8-plugin")
+    ProjectRef(uri("git://github.com/n8han/giter8#9c0f9fc"), "giter8-plugin")
 }
